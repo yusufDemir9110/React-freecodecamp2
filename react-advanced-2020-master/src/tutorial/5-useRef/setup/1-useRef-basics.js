0362f3f4bfbb9881e14cb3bfb3e0fd76b008147c
yusuf Demir <yusuf.ferrum@gmail.com>
2022-04-27 13:53:21 +0200

    737

--- react-advanced-2020-master/src/tutorial/5-useRef/setup/1-useRef-basics.js
+++ react-advanced-2020-master/src/tutorial/5-useRef/setup/1-useRef-basics.js
@@ -1,11 +1,33 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from "react";
+import ControlledInputs from "../../4-forms/setup/2-multiple-inputs";
 
 // preserves value
 // DOES NOT trigger re-render
 // target DOM nodes/elements
 
 const UseRefBasics = () => {
-  return <h2>useRef</h2>;
+  const refContainer = useRef(null);
+  const divContainer = useRef(null);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(refContainer.current.value);
+    console.log(divContainer.current);
+  };
+
+  useEffect(() => {
+    console.log(refContainer.current);
+    refContainer.current.focus();
+  });
+
+  return (
+    <div>
+      <form className="form" onSubmit={handleSubmit}>
+        <input type="text" ref={refContainer} />
+        <button type="submit">submit</button>
+      </form>
+      <div ref={divContainer}>hello world</div>
+    </div>
+  );
 };
 
 export default UseRefBasics;
--- react-advanced-2020-master/src/tutorial/6-useReducer/setup/index.js
+++ react-advanced-2020-master/src/tutorial/6-useReducer/setup/index.js
@@ -1,10 +1,42 @@
-import React, { useState, useReducer } from 'react';
-import Modal from './Modal';
-import { data } from '../../../data';
+import React, { useState, useReducer } from "react";
+import Modal from "./Modal";
+import { data } from "../../../data";
 // reducer function
-
+const reducer = (state, action) => {};
+const defaultState = {
+  people: [],
+  isModalOpen: false,
+  modalContent: "",
+};
 const Index = () => {
-  return <h2>useReducer</h2>;
+  const [name, setName] = useState("");
+  const [state, dispatch] = useReducer(reducer, defaultState);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (name) {
+    } else {
+    }
+  };
+  return (
+    <div>
+      {state.isModalOpen && <Modal modalContent={state.modalContent} />}
+      <form onSubmit={handleSubmit} className="form">
+        <div>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <button type="submit">add</button>
+      </form>
+      {state.people.map((person) => (
+        <div key={person.id}>
+          <h4>{person.name}</h4>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Index;
