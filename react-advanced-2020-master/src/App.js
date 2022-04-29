def9192e94da094422942c3267062f7dd1e33e02
yusuf Demir <yusuf.ferrum@gmail.com>
2022-04-29 14:08:18 +0200

    906

--- react-advanced-2020-master/src/App.js
+++ react-advanced-2020-master/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Setup from "./tutorial/8-useContext/setup/1-context-api";
+import Setup from "./tutorial/11-react-router/setup";
 function App() {
   return (
     <div className="container">
